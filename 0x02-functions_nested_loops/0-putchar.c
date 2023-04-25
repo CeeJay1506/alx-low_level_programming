82c08d7f6423c48f3106ab656896a48a5d941385
Chisom Nweke <judithnweke82@gmail.com>
2023-04-25 04:22:17 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,20 @@
-#include "holberton.h"
+#include"main.h"
+
 /**
-* main - Prints Holberton
-*
-* Return: 0
-*/
+ * main - program entry
+ *
+ * Description: this prints _putchar using putchar
+ *
+ * Return: always 0
+ */
 int main(void)
 {
-	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
-	int count, sz;
+	char str[] = "_putchar";
+	int ch;
 
-	sz = sizeof(str) / sizeof(int);
-	for (count = 0; count < sz; count++)
-	{	
-		_putchar(str[count]);
-	}
+	for (ch = 0; ch < 8; ++ch)
+		_putchar(str[ch]);
 	_putchar('\n');
+
 	return (0);
 }
